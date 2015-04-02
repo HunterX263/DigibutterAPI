3e4a71a2a249a9c59049239156f6cc78cabc78ae
HunterX263 <hunterx@g33k.org.uk>
2015-04-02 15:35:51 +0100

    Added initial Game properties.

--- DigibutterApi/src/uk/org/g33k/digibutterapi/betalands/Game.java
+++ DigibutterApi/src/uk/org/g33k/digibutterapi/betalands/Game.java
@@ -1,5 +1,10 @@
 package uk.org.g33k.digibutterapi.betalands;
 
-public class Game {
+import java.util.ArrayList;
 
+public class Game {
+	private ArrayList<Entity> entities;
+
+	private int tileSize = 8;
+	private int[][] map;
 }
