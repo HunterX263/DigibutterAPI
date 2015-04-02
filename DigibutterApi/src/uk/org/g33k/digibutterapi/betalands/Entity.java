574c9cb8def0dd61f1e717394e9bd0c5d50d621a
HunterX263 <hunterx@g33k.org.uk>
2015-04-02 15:47:19 +0100

    Added initial properties and methods to Entity class.

--- DigibutterApi/src/uk/org/g33k/digibutterapi/betalands/Entity.java
+++ DigibutterApi/src/uk/org/g33k/digibutterapi/betalands/Entity.java
@@ -1,5 +1,31 @@
 package uk.org.g33k.digibutterapi.betalands;
 
-public class Entity {
+import java.awt.Point;
 
+public class Entity {
+	private Point size = new Point(16, 16);
+	private Point pos = new Point(0, 0);
+	private Point last = new Point(0, 0);
+	private Point vel = new Point(0, 0);
+	private Point accel = new Point(0, 0);
+	private Point maxVel = new Point(100, 100);
+	
+	private int gravityFactor = 1;
+	private int bounciness = 0;
+	private int health = 10;
+	
+	public void update()
+	{
+		
+	}
+	
+	private void getNewVelocity()
+	{
+		
+	}
+	
+	private void handleMovementTrace()
+	{
+		
+	}
 }
