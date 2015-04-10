1f8abed74176618c039af06582f25cc86f53be71
HunterX263 <hunterx@g33k.org.uk>
2015-04-10 11:31:44 +0100

    Assorted changes.

--- DigibutterApi/src/uk/org/g33k/digibutterapi/betalands/Entity.java
+++ DigibutterApi/src/uk/org/g33k/digibutterapi/betalands/Entity.java
@@ -8,20 +8,55 @@ public class Entity {
 	private Point last = new Point(0, 0);
 	private Point vel = new Point(0, 0);
 	private Point accel = new Point(0, 0);
+	private Point friction = new Point(0, 0);
 	private Point maxVel = new Point(100, 100);
 	
 	private int gravityFactor = 1;
 	private int bounciness = 0;
 	private int health = 10;
 	
-	public void update()
+	public void update(int gravity, int tick)
 	{
+		last.x = pos.x;
+		last.y = pos.y;
 		
+		vel.y += gravity * tick * gravityFactor;
+		vel.x = getNewVelocity(vel.x, accel.x, friction.x, maxVel.x, tick);
+		vel.y = getNewVelocity(vel.y, accel.y, friction.y, maxVel.y, tick);
+		
+		int mx = vel.x * tick;
+		int my = vel.y * tick;
+		//TODO
+		
+		handleMovementTrace();
 	}
 	
-	private void getNewVelocity()
+	private int getNewVelocity(int vel, int accel, int friction, int max, int tick)
 	{
-		
+		if (accel > 0)
+		{
+			return Math.max(-max, Math.min(max, vel + accel * tick));
+		}
+		else if (friction > 0)
+		{
+			int delta = friction * tick;
+			if (vel - delta > 0)
+			{
+				return vel - delta;
+			}
+			else if (vel + delta < 0)
+			{
+				return vel + delta;
+			}
+			else
+			{
+				return 0;
+			}
+		}
+		else
+		{
+			return Math.max(-max, Math.min(max, vel));
+		}
 	}
 	
 	private void handleMovementTrace()
--- DigibutterApi/src/uk/org/g33k/digibutterapi/betalands/Game.java
+++ DigibutterApi/src/uk/org/g33k/digibutterapi/betalands/Game.java
@@ -1,18 +1,53 @@
 package uk.org.g33k.digibutterapi.betalands;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
+
+import org.json.simple.JSONObject;
 
 public class Game {
 	private ArrayList<Entity> entities;
+	private int gravity = 1; //TODO
+	long lastTick;
+	int ticksPerSecond = 60;
 
 	private int tileSize = 8;
-	private int[][] map;
+	private HashMap<String, Integer> map = new HashMap<String, Integer>();
 	
 	public void update()
 	{
+		Date current = new Date();
+		int ticks;
+		if (lastTick == -1)
+		{
+			ticks = 1;
+		}
+		else
+		{
+			long diff = current.getTime() - lastTick;
+			ticks = (int)Math.round(((double)diff / 1000d) * ticksPerSecond);
+		}
+		lastTick = current.getTime();
+		
 		for (Entity e : entities)
 		{
-			e.update();
+			e.update(gravity, ticks);
 		}
+	}
+	
+	public int getTile(int x, int y)
+	{
+		return -1;
+	}
+	
+	public void setTile(int x, int y, int val)
+	{
+		
+	}
+	
+	public void loadLevel(JSONObject bits)
+	{
+		
 	}
 }
